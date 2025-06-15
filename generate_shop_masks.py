d00e50ed6e8e6cb55411fda6389daf807a09df9d
Ice424 <e4elliotd@gmail.com>
2025-06-15 12:44:27 +0100

    THE WORLD (not revolving(yes its a jojos))

--- generate_shop_masks.py
+++ generate_shop_masks.py
@@ -60,7 +60,7 @@ def generate_shop_masks():
     file = open(os.path.join(DATA, "main", "shop", "mask.mcfunction"), "w", encoding='utf-8')
     file.write("""data modify storage ui mask set value [{Slot:0b,id:"minecraft:barrier","components":{"custom_model_data": 6, "custom_name": "{\\"text\\": \\"Back\\", \\"color\\": \\"red\\",\\"italic\\": false}","minecraft:custom_data":{ui_item:{cmd:"function ui:menu/main/shop/close_shop"}}}}]""")
     file.write("\n\n")
-    file.write("""data modify storage ui mask insert 0 value {Slot: 22b, id: "minecraft:barrier", "components": {"lore:["{\\"color\\":\\"dark_aqua\\",\\"font\\":\\"chill:essence\\",\\"italic\\":false,\\"text\\":\\"Re-roll slots 10Θ\\"}"],custom_name:"{\\"color\\":\\"gray\\",\\"italic\\":false,\\"text\\":\\"Refresh\\"}", "minecraft:custom_data": {ui_item: {cmd: "function ui:menu/main/reset_slots"}}}}""")
+    file.write("""data modify storage ui mask insert 0 value {Slot: 22b, id: "minecraft:barrier", "components": {"lore":["{\\"color\\":\\"dark_aqua\\",\\"font\\":\\"chill:essence\\",\\"italic\\":false,\\"text\\":\\"Re-roll slots 10Θ\\"}"],custom_name:"{\\"color\\":\\"gray\\",\\"italic\\":false,\\"text\\":\\"Refresh\\"}", "minecraft:custom_data": {ui_item: {cmd: "function ui:menu/main/reset_slots"}}}}""")
     file.write("\n\n")
     #file.write("""data modify storage ui mask insert 0 value {Slot: 23b, id: "minecraft:stick", "components": {"custom_model_data": 5, custom_name:"{\\"color\\":\\"dark_gray\\",\\"italic\\":false,\\"text\\":\\"Test\\"}", "minecraft:custom_data": {ui_item: {cmd: "function ui:menu/main/shop_confirm/open {predicate: 50}"}}}}""")
     file.write("\n\n")
